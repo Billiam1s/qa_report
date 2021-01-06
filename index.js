8777e5ba79358e50ae1e967ed766a57fb65b5b70
Billiam1s <billy.addington@ft.com>
2021-01-06 15:04:30 +0000

    changed port

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/', express.urlencoded({ extended: true }), (req, res) => {
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 8000;
+    port = 3000;
 }
 
 app.listen(port, () => {
